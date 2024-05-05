e87a7fb2a98ec0bc08a9426af9d7b5642a4c908e
Avi-koushal <kavishek1999@gmail.com>
2024-05-06 01:03:24 +0530

    job-card

--- src/jobs/jobsCard.jsx
+++ src/jobs/jobsCard.jsx
@@ -1,17 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-function jobsCard() {
+function JobsCard() {
+    const [jobData, setJobData] = useState([]);
 
-    
-  return (
-    <>
-    <div className="row">
-      <div className='' style={{textAlign: 'center'}}>
-      Job Search
-      </div>
-    </div>
-    </>
-  );
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const requestOptions = {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                };
+                const response = await fetch('https://api.weekday.technology/adhoc/getSampleJdJSON', requestOptions);
+                if (!response.ok) {
+                    throw new Error('Network error');
+                }
+                const data = await response.json();
+                setJobData(data);
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        };
+
+        fetchData();
+        return () => { };
+    }, []);
+
+    console.log(jobData)
+
+    return (
+        <div>
+            <div className='job-head'>Job Search{jobData.length}</div>
+            {jobData.length}
+            {jobData.length > 0 ? (
+                jobData.map((job, index) => (
+                    <div key={index} className='col-3 job-card'>
+                        <div>
+                            <span>Job title: {job.jobRole}</span>
+                        </div>
+                        <div>
+                            <span>Company name: {job.companyName}</span>
+                        </div>
+                        <div>
+                            <span>Location: {job.location}</span>
+                        </div>
+                        <div>
+                            <span>Job description: {job.jobDetailsFromCompany}</span>
+                        </div>
+                        <div>
+                            <span>Experience required: {job.minExp} - {job.maxExp} years</span>
+                        </div>
+                        <div className='apply'>
+                            <button>Apply</button>
+                        </div>
+                    </div>
+                ))
+            ) : (
+                <div>Loading...</div>
+            )}
+        </div>
+    );
 }
 
-export default jobsCard;
+export default JobsCard;
